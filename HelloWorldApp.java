8565fbfe1d7a436518297ff75ffbe48c5c21cbd6
rmusale <32094384+rmusale@users.noreply.github.com>
2019-08-28 15:47:09 +0530

    Update HelloWorldApp.java for Azure Pipelines

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,10 +1,6 @@
-//*
-// * The HelloWorldApp class implements an application that
- //* simply prints "Hello World!" to standard output.
- //*
 
- 
 public class HelloWorldApp {
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");// Display the string.
         System.out.println("First Jenkines deplyed by Rushikesh Musale");
